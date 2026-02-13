4e2aac58d6d21099407389ce7610e3536df74ed1
DeltaboxStore <deltaboxstore.custsvc@gmail.com>
2026-02-13 13:19:13 +0800

    initial

--- middleware.js
+++ middleware.js
@@ -115,7 +115,7 @@ async function applyAdminSessionCookie(response) {
 
 function applySecurityHeaders(response) {
   response.headers.set('X-Content-Type-Options', 'nosniff');
-  response.headers.set('X-Frame-Options', 'DENY');
+  response.headers.set('X-Frame-Options', 'SAMEORIGIN');
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
   response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
   response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
